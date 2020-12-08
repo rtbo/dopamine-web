c49368378d8fe2b7951b8c76a4c5f743c22c5724
Remi Thebault <remi.thebault@gmail.com>
2020-12-09 00:06:21 +0100

    server: logger

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -2,6 +2,7 @@ import cors from '@koa/cors'
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import helmet from 'koa-helmet'
+import logger from 'koa-logger'
 
 import { MongoClient } from 'mongodb'
 import env from './env'
@@ -12,6 +13,7 @@ const app = new Koa()
 app.use(helmet())
 app.use(cors())
 app.use(bodyParser())
+app.use(logger())
 
 const v1Router = buildApiV1Router()
 
